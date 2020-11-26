65f7f4ca0bc01bf570597f995e4c90d1fd165e3e
satiya3577@gmail.com <satiya3577@gmail.com>
2020-11-26 17:02:49 +0700

    Worktime Report Update

--- pages/worktime/worktime_report.php
+++ pages/worktime/worktime_report.php
@@ -37,16 +37,22 @@ $thai_month_arr=array(
             </select>
             ปี
             <select id="year_check" class="select-single" name="year_check">
-                <?php $data_year=intval(date("Y",strtotime("-2 year")));?>
-                <?php for($i=0;$i<=5;$i++){ ?>
+                <?php $data_year=intval(date("Y",strtotime("-1 year")));?>
+                <?php for($i=0;$i<=2;$i++){ ?>
                 <option value="<?=$data_year+$i?>"
                     <?=((isset($_POST['year_check']) && $_POST['year_check']==intval($data_year+$i)) || (!isset($_POST['year_check']) && date("Y")==intval($data_year+$i)))?" selected":""?>>
                     <?=intval($data_year+$i)+543?>
                 </option>
                 <?php } ?>
             </select>
-            <button id="showData" name="showData" class="btn btn-sm btn-dark" type="submit"><i
-                    class="fa fa-search"></i></button>
+            <?php $fetchDept = $hr->getDepartment();?>
+            <select id="dept_check" class="select-single" name="dept_check">
+                <option value="0" selected>เลือกทั้งหมด</option>
+                <?php foreach ($fetchDept as $ds){ echo "<option value='".$ds['dept_id']."'> - ".$ds['dept_name']."</option>";} ?>
+            </select>
+            <button id="showData" name="showData" class="btn btn-sm btn-dark" type="submit">
+                <i class="fa fa-search"></i> ค้นหา
+            </button>
         </form>
         <div class="table-responsive">
             <?php
@@ -55,19 +61,22 @@ $thai_month_arr=array(
                 $use_month_check = $date_data_check;        
                 $start_date_check = $date_data_check."01";
                 $end_date_check = $date_data_check.$num_month_day;
-
+                $dept = $_POST['dept_check'];
+                if($dept==0){$finddept="";}else{$finddept="AND tb_employee.emp_dept = $dept";}
                 $sql = "SELECT *,DATE_FORMAT(work_time,'%Y-%m-%d') as date_s 
                         FROM tb_worktime
                         LEFT JOIN tb_employee ON tb_worktime.emp_barcode = tb_employee.emp_barcode
-                        WHERE tb_worktime.work_time >= '".$start_date_check."' AND tb_worktime.work_time <= '".$end_date_check."' ORDER BY tb_employee.emp_barcode ASC";
-                // echo $sql;
+                        WHERE tb_worktime.work_time >= '".$start_date_check."' 
+                        AND tb_worktime.work_time <= '".$end_date_check."' $finddept
+                        ORDER BY tb_employee.emp_barcode ASC";
                 $result = $mysqli->query($sql);
+
                 if($result){
                     while($row = $result->fetch_assoc()){
                         if(isset($data_arr[$row['emp_name']][$row['date_s']])){
-                            $data_arr[$row['emp_name']][$row['date_s']]+=1;
+                            $data_arr[$row['emp_name']][$row['date_s']]+=$row['work_status'];
                         }else{
-                            $data_arr[$row['emp_name']][$row['date_s']]=1;
+                            $data_arr[$row['emp_name']][$row['date_s']]=$row['work_status'];
                         }
                     }
                 }
@@ -82,7 +91,7 @@ $thai_month_arr=array(
                         <?php 
                         $days = date('w', strtotime("".$_POST['year_check']."-".$_POST['month_check']."-".$i.""));
                             if($days == 0 or $days == 6){
-                                $bg = "red";
+                                $bg = "black";
                                 $fg = "white";
                             }else{
                                 $bg = "";
@@ -109,12 +118,23 @@ $thai_month_arr=array(
                     <td class="text-center">
                         <?php
                             $key_date = date("Y-m-d",strtotime($start_date_check." +$i day"));
-                            if(isset($v_data["$key_date"])){
-                                if($v_data["$key_date"]==1){
-                                    echo "<i class='fa fa-check text-success'></i>";
-                                }else{
-                                    echo "<i class='far fa-clock text-danger'></i>";
-                                }
+                            if(isset($v_data["$key_date"])&&$v_data["$key_date"]==1){
+                                echo "<span class='badge badge-success' style='font-size:10px;'>ช</span>";
+                            }
+                            if(isset($v_data["$key_date"])&&$v_data["$key_date"]==3){
+                                echo "<span class='badge badge-primary' style='font-size:10px;'>บ</span>";
+                            }
+                            if(isset($v_data["$key_date"])&&$v_data["$key_date"]==4){
+                                echo "<span class='badge badge-dark' style='font-size:10px;'>ช/บ</span>";
+                            }
+                            if(isset($v_data["$key_date"])&&$v_data["$key_date"]==5){
+                                echo "<span class='badge badge-danger' style='font-size:10px;'>ด</span>";
+                            }
+                            if(isset($v_data["$key_date"])&&$v_data["$key_date"]==6){
+                                echo "<span class='badge badge-info' style='font-size:10px;'>ช/ด</span>";
+                            }
+                            if(isset($v_data["$key_date"])&&$v_data["$key_date"]==9){
+                                echo "<span class='badge badge-warning' style='font-size:10px;'>ช/บ/ด</span>";
                             }
                         ?>
                     </td>
@@ -122,15 +142,13 @@ $thai_month_arr=array(
                 </tr>
                 <?php } } ?>
             </table>
-            <i class='fa fa-check text-success'></i> : เข้างานปกติ / เวรเช้า <br>
-            <i class='far fa-clock text-danger'></i> : ขึ้นเวร / OT (เวรบ่าย , เวรดึก)
         </div>
     </div>
 </div>
 <script>
     $(document).ready(function () {
         $('.select-single').select2({
-            width: '10%'
+            width: '20%'
         });
     });
 </script>
